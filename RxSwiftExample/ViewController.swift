02e038f30d074554f553385033d918130fc4c8cf
Aline Borges <alinekborges@gmail.com>
2018-01-31 17:53:07 -0200

    Initial Commit

---
+++ RxSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftExample
+//
+//  Created by Aline Borges on 31/01/18.
+//  Copyright © 2018 Aline Borges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
